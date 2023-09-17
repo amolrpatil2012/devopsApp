854f7fe43c3fd13799ea6deeb81b5b72949579c1
amolpatil <amolrpatil2012@gmail.com>
2023-09-18 04:48:16 +0530

    First Commit

---
+++ src/test/java/devops/MainTest.java
@@ -0,0 +1,15 @@
+package devops;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void test() {
+		System.out.println("In Testing Framework");
+		assertEquals(10,10);
+	}
+
+}
